eea99918cff2b281bb1c46c2968d6f309cad84e3
valentinaiori <valentinaiori@live.it>
2022-09-29 09:40:17 +0200

    Add apiKey gitignore

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HeaderComponent/>
+    <MainComponent/>
+  </div>
+</template>
+
+<script>
+  import HeaderComponent from './components/HeaderComponent.vue';
+  import MainComponent from './components/MainComponent.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent,
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
