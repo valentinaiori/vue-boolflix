1aaa8a4f274fe9e868924d2611919efdcfdfc3ca
valentinaiori <valentinaiori@live.it>
2022-09-29 11:31:01 +0200

    Add milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,60 @@
 <template>
   <div id="app">
-    <HeaderComponent/>
-    <MainComponent/>
+    <div>
+    <input type="text" v-model="query"> <button @click="search">Cerca</button>
+</div>
+<div class="container-card">
+    <div class="card" v-for="movie in movies" :key="movie.id">
+      <p>Title: {{ movie.title }}</p>
+      <p> Original title: {{ movie.original_title }}</p>
+      <p> Language: {{ movie.original_language }}</p>
+      <p> Vote: {{ movie.vote_average }}</p>
+    </div>
+</div>
   </div>
+  
 </template>
 
 <script>
-  import HeaderComponent from './components/HeaderComponent.vue';
-  import MainComponent from './components/MainComponent.vue';
+
+import axios from 'axios';
+import {apiKey} from '@/env';
 
 
 export default {
   name: 'App',
-  components: {
-    HeaderComponent,
-    MainComponent,
-    
+
+
+  data(){
+    return{
+      query: '',
+      movies: [],
+    }
+
+  },
+  mounted(){
+    this.queryApi('ritorno al fut')
+
+  }, 
+  methods: {
+    search(){
+      this.queryApi(this.query)
+    }, 
+
+    queryApi(textToSearch){
+      axios.get( `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${textToSearch}&language=it-IT`)
+      .then((response)=>{
+        console.log(response);
+        if( response.status === 200){
+          this.movies = response.data.results;
+        }
+      })
+    }
   }
+
 }
 </script>
 
-<style lang="scss">
+<style>
 
 </style>
